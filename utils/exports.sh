19d671468384e81747622f515fc94f1c8d6e2155
Rafa Gómez <dotzecker@gmail.com>
2015-10-05 20:57:07 +0200

    [exports] Add ~/bin

--- utils/exports.sh
+++ utils/exports.sh
@@ -1,3 +1,4 @@
 #export PATH=$HOME/bin:/usr/local/bin:$PATH
 #export PATH=$HOME/Development/adt-bundle/platform-tools:$HOME/Development/adt-bundle/tools:$PATH
 set fish_user_paths $fish_user_paths ~/.composer/vendor/bin
+set fish_user_paths $fish_user_paths ~/bin
