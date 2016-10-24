747e7ee89b47be401bfd1ab9a2d77d7654892cef
Rafa Gómez <rgomezcasas@gmail.com>
2016-10-24 15:37:54 +0200

    Update intellij settings

--- zsh/aliases/aws.sh
+++ zsh/aliases/aws.sh
@@ -1,5 +1,5 @@
 function s {
     server_to_connect=$(ec2s $argv | percol --prompt='<green>Select the server to connect:</green> %q')
     server_ip=$(echo $server_to_connect | awk '{print $2}')
-    ssh -l $MY_SSH_USERNAME $server_ip
+    ssh -i $MY_PEM_FILE ec2-user@$server_ip
 }
