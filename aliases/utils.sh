ef1ace9d24ba753dd06eaa710625ead29ae831d1
Rafa Gómez <rgomezcasas@gmail.com>
2017-03-08 16:19:04 +0100

    Update intellij settings

--- aliases/utils.sh
+++ aliases/utils.sh
@@ -1,4 +1,3 @@
-alias j='z'
 alias aux='ps uax'
 alias brwe='brew'
 alias edithosts='sudo vim /etc/hosts'
@@ -7,15 +6,17 @@ alias copy_ssh_key='xclip -sel clip < ~/.ssh/id_rsa.pub'
 alias count_files_recursive='find . -type f -print | wc -l'
 alias fcb='soccer --team=FCB --time=100'
 alias fuck!='sudo $history[1]'
+alias j='z'
 alias k='kill -9'
 alias lfp='soccer --standings --league=LLIGA'
+alias r='realpath'
 alias reveal='open .'
 alias size_of_the_current_directory='du -ch | grep total'
 alias stt='subl .'
 alias watch_number_of_files='watch -n1 "find . -type f -print | wc -l"'
 alias code='cd ~/Code'
 
-alias outdated_apps='mas outdated; brew outdated'
+alias outdated_apps='mas outdated; brew outdated'x
 alias update_brew_apps='brew outdated | awk "{print $1}" | xargs brew upgrade'
 alias update_apps='brew outdated | awk "{print $1}" | xargs brew upgrade; mas upgrade'
 
