38a3ef2f077be4406d1891a20fcbb7cb6e2fdaaf
Rafa Gómez <rgomezcasas@gmail.com>
2017-06-13 12:21:54 +0200

    Update intellij colors

--- fish/exports/exports.fish
+++ fish/exports/exports.fish
@@ -1,6 +1,6 @@
 set MY_SSH_USERNAME 'rafa.gomez'
 
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home/
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/
 set -x SBT_OPTS "-Xms512M -Xmx1024M -Xss6M -XX:MaxMetaspaceSize=512m -XX:+CMSClassUnloadingEnabled"
 
 set -U fish_user_paths ~/bin ~/.dotfiles/bin ~/.composer/vendor/bin $JAVA_HOME
--- zsh/exports/exports.sh
+++ zsh/exports/exports.sh
@@ -1,5 +1,5 @@
 export MY_SSH_USERNAME='rafa.gomez'
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/
 
 export PATH=$PATH:~/bin:~/.composer/vendor/bin:$DOTFILES_PATH/bin:/usr/local/sbin:JAVA_HOME
