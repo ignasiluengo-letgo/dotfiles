c688dbdbbf084339c539ee5d0a86eb2a348b6524
Rafa Gómez <rgomezcasas@gmail.com>
2017-07-30 11:43:22 +0200

    Use fzf for history search in fish

--- fish/exports/exports.fish
+++ fish/exports/exports.fish
@@ -4,3 +4,5 @@ set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Hom
 set -x SBT_OPTS "-Xms512M -Xmx1024M -Xss6M -XX:MaxMetaspaceSize=512m -XX:+CMSClassUnloadingEnabled"
 
 set -U fish_user_paths ~/bin ~/.dotfiles/bin ~/.composer/vendor/bin $JAVA_HOME
+
+set -x FZF_DEFAULT_OPTS '--color=bg+:24 --reverse'
