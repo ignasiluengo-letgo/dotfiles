360c86aef7ddb801000ae4dcfcebfa97e6961351
Rafa Gómez <rgomezcasas@gmail.com>
2017-08-09 12:24:44 +0200

    Add exa for ls

--- aliases/utils.sh
+++ aliases/utils.sh
@@ -1,3 +1,15 @@
+# Dir
+alias p='pwd'
+alias c='cd'
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ll='exa -l --git'
+alias la='exa -la --git'
+alias ctree='exa -T'
+
+# Others
 alias aux='ps uax'
 alias brwe='brew'
 alias edithosts='sudo vim /etc/hosts'
@@ -6,7 +18,7 @@ alias copy_ssh_key='xclip -sel clip < ~/.ssh/id_rsa.pub'
 alias count_files_recursive='find . -type f -print | wc -l'
 alias fcb='soccer --team=FCB --time=100'
 alias fuck!='sudo $history[1]'
-alias j='z'
+alias j='fasd_cd -d'
 alias k='kill -9'
 alias lfp='soccer --standings --league=LLIGA'
 alias r='realpath'
@@ -16,6 +28,7 @@ alias stt='subl .'
 alias watch_number_of_files='watch -n1 "find . -type f -print | wc -l"'
 alias code='cd ~/Code'
 alias t='time'
+
 
 alias catimg='imgcat'
 
