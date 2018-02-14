dbbc241e3c175bee1e6d73e186c81e21ab9ca81b
Rafa Gómez <rgomezcasas@gmail.com>
2018-02-15 00:25:34 +0100

    [mac] Update brew apps

--- mac/generate-brew-list.sh
+++ mac/generate-brew-list.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-brew list > ~/.dotfiles/etc/mac/brew/brew-installed.md
-brew cask list > ~/.dotfiles/etc/mac/brew/brew-cask-installed.md
+brew list > ~/.dotfiles/mac/brew/brew-installed.md
+brew cask list > ~/.dotfiles/mac/brew/brew-cask-installed.md
