2f8ee35db3205d9b64c85175721f6dadedd20d66
Rafa Gómez <rgomezcasas@gmail.com>
2018-02-15 00:21:57 +0100

    [git] Extract aliases to binaries

--- console/_aliases/git.sh
+++ console/_aliases/git.sh
@@ -1,7 +1,7 @@
 alias ga='git add -A'
 alias gc='git c'
 alias gn='git n'
-alias gam='git a'
+alias gam='git amend'
 
 alias gco='git checkout'
 alias gd='git diff --color'
@@ -9,6 +9,7 @@ alias gl='git log --graph --pretty=format:"%Cred%h%Creset %C(yellow)%d%Creset %a
 alias gs='git status -sb'
 alias gf='git fetch --all -p'
 alias gp='git push'
+alias gfp='git push -f'
 alias gpl='git pull'
 alias gmm='git merge master'
 alias gpom='git pull origin master'
